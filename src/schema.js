d1e360c12c22140e950f82cf88e250af3726e105
Johannes Koch <johannes.koch@avenga.com>
2021-08-23 15:05:41 +0200

    Added: error_handler for beta_oauth2 and beta_oidc

--- src/schema.js
+++ src/schema.js
@@ -9,12 +9,14 @@ const blocks = {
         labelled: true
     },
     error_handler: {
-        parents: ['basic_auth', 'jwt', 'saml'],
+        parents: ['basic_auth', 'jwt', 'saml', 'beta_oauth2', 'beta_oidc'],
         labelled: false,
         labels: {
             'basic_auth': ['basic_auth', 'basic_auth_credentials_missing'],
             'jwt':        ['jwt', 'jwt_token_expired', 'jwt_token_invalid', 'jwt_token_missing'],
-            'saml':       ['saml2']
+            'saml':       ['saml2'],
+            'beta_oauth2': ['oauth2'],
+            'beta_oidc':   ['oauth2'],
         }
     },
     files: {
