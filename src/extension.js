fc2604d27d24f7d9525e682693798bb06bb37608
Joe Afflerbach <joe.afflerbach@avenga.com>
2021-10-07 14:04:52 +0200

    Go to definition for (disable_)access_control and backend references

--- src/extension.js
+++ src/extension.js
@@ -3,8 +3,9 @@
 Object.defineProperty(exports, "__esModule", { value: true })
 
 const Completion = require("./completion.js")
+const Definition = require("./definition.js")
 
 exports.activate = (context) => {
-	context.subscriptions.concat(Completion.providers)
+	context.subscriptions.concat(Completion.providers, Definition.providers)
 	console.info("Extension loaded: Couper Configuration")
 }
