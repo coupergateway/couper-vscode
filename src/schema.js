df787b8a4d53396833aab544a14e1c6330b09bd1
Johannes Koch <johannes.koch@avenga.com>
2021-12-22 11:41:20 +0100

    tab completion for sequence and unexpected_status error types

--- src/schema.js
+++ src/schema.js
@@ -43,7 +43,7 @@ const blocks = {
         _labelsForParent: {
             'api':         ['beta_scope', 'beta_insufficient_scope', 'beta_operation_denied'],
             'basic_auth':  ['basic_auth', 'basic_auth_credentials_missing'],
-            'endpoint':    ['beta_scope', 'beta_insufficient_scope', 'beta_operation_denied'],
+            'endpoint':    ['beta_scope', 'beta_insufficient_scope', 'beta_operation_denied', 'sequence', 'unexpected_status'],
             'jwt':         ['jwt', 'jwt_token_expired', 'jwt_token_invalid', 'jwt_token_missing'],
             'saml':        ['saml2'],
             'beta_oauth2': ['oauth2'],
