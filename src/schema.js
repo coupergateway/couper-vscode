4fac2cdae9d8a0ad7b6144f8c318962c3aae4848
Joe Afflerbach <joe.afflerbach@avenga.com>
2022-03-21 11:41:23 +0100

    Example references

--- src/schema.js
+++ src/schema.js
@@ -6,11 +6,13 @@ const blocks = {
     api: {
         parents: ['server'],
         description: "Bundles endpoints under a certain `base_path`.",
+        examples: ['api-proxy'],
         labels: [null, DEFAULT_LABEL]
     },
     backend: {
         parents: ['beta_oauth2', 'definitions', 'jwt', 'oauth2', 'oidc', 'proxy', 'request'],
         description: "Defines the connection to a local/remote backend service.",
+        examples: ['backend-configuration'],
         labelled: (parentBlockName) => {
             return parentBlockName === "definitions"
         }
@@ -45,6 +47,7 @@ const blocks = {
     },
     error_handler: {
         parents: ['api', 'basic_auth', 'beta_oauth2', 'endpoint', 'jwt', 'oidc', 'saml'],
+        examples: ['error-handling-ba', 'sequences'],
         labels: (parentBlockName) => {
             return [null].concat(blocks.error_handler._labelsForParent[parentBlockName])
         },
@@ -61,36 +64,43 @@ const blocks = {
     files: {
         parents: ['server'],
         description: "Configures file serving.",
+        examples: ['simple-fileserving', 'spa-serving'],
         labelled: false
     },
     jwt: {
         parents: ['definitions'],
         description: "Configures a JSON Web Token access control.",
+        examples: ['jwt-access-control', 'creating-jwt'],
         labelled: true
     },
     jwt_signing_profile: {
         parents: ['definitions'],
         description: "Configure a JSON Web Token signing profile which is referenced in the `jwt_sign()` function.",
+        examples: ['creating-jwt'],
         labelled: true
     },
     oauth2: {
         parents: ['backend'],
         description: "Configures the OAuth2 Client Credentials flow to request a bearer token for its backend request.",
+        examples: ['oauth2-client-credentials'],
         labelled: false
     },
     oidc: {
         parents: ['definitions'],
         description: "An access control for an OIDC Authorization Code Grant Flow redirect endpoint.",
+        examples: ['oidc'],
         labelled: true
     },
     openapi: {
         parents: ['backend'],
         description: "Configures the OpenAPI 3 validation of messages to and from the origin.",
+        examples: ['backend-validation'],
         labelled: false
     },
     proxy: {
         parents: ['endpoint', 'error_handler'],
         description: "Executes a proxy request to a backend service.",
+        examples: ['api-proxy', 'custom-requests', 'multiple-requests'],
         labels: [null, DEFAULT_LABEL]
     },
     request: {
@@ -102,11 +112,13 @@ const blocks = {
     response: {
         parents: ['endpoint', 'error_handler'],
         description: "Sends a client response.",
+        examples: ['static-responses'],
         labelled: false
     },
     saml: {
         parents: ['definitions'],
         description: "An access control for a SAML ACS endpoint.",
+        examples: ['saml'],
         labelled: true
     },
     server: {
@@ -133,6 +145,7 @@ const attributes = {
     access_control: {
         parents: ['api', 'endpoint', 'files', 'server', 'spa'],
         description: "Secures the current block context with a predefined access control.",
+        examples: ['jwt-access-control'],
         type: 'array'
     },
     accept_forwarded_url: {
@@ -145,6 +158,7 @@ const attributes = {
     },
     add_query_params: {
         parents: ['backend', 'endpoint', 'error_handler', 'proxy'],
+        examples: ['query'],
         type: 'map'
     },
     add_request_headers: {
@@ -170,13 +184,15 @@ const attributes = {
     },
     array_attributes: {
         parents: ['saml'],
+        examples: ['saml'],
         type: 'array'
     },
     authorization_endpoint: {
         parents: ['beta_oauth2']
     },
     backend: { // label reference
-        parents: ['beta_oauth2', 'jwt', 'oauth2', 'oidc', 'proxy', 'request']
+        parents: ['beta_oauth2', 'jwt', 'oauth2', 'oidc', 'proxy', 'request'],
+        examples: ['backend-configuration']
     },
     base_path: {
         parents: ['api', 'files', 'server', 'spa']
@@ -216,23 +232,28 @@ const attributes = {
         parents: ['request', 'response']
     },
     bootstrap_file: {
-        parents: ['spa']
+        parents: ['spa'],
+        examples: ['spa-serving']
     },
     ca_file: {
       parents: ['settings']
     },
     claims: {
         parents: ['jwt', 'jwt_signing_profile'],
+        examples: ['jwt-access-control'],
         type: 'map'
     },
     client_id: {
-        parents: ['beta_oauth2', 'oauth2', 'oidc']
+        parents: ['beta_oauth2', 'oauth2', 'oidc'],
+        examples: ['oauth2-client-credentials', 'oidc']
     },
     client_secret: {
-        parents: ['beta_oauth2', 'oauth2', 'oidc']
+        parents: ['beta_oauth2', 'oauth2', 'oidc'],
+        examples: ['oauth2-client-credentials', 'oidc']
     },
     configuration_url: {
-        parents: ['oidc']
+        parents: ['oidc'],
+        examples: ['oidc']
     },
     configuration_ttl: {
         parents: ['oidc']
@@ -245,6 +266,7 @@ const attributes = {
     },
     custom_log_fields: {
         parents: ['api', 'backend', 'endpoint', 'error_handler', 'files', 'server', 'spa', 'jwt', 'basic_auth', 'saml', 'beta_oidc', 'beta_oauth2'],
+        examples: ['custom-logging', 'sequences'],
         type: 'map'
     },
     default_port: {
@@ -272,18 +294,22 @@ const attributes = {
         type: 'boolean'
     },
     document_root: {
-        parents: ['files']
+        parents: ['files'],
+        examples: ['simple-fileserving', 'spa-serving']
     },
     environment_variables: {
         parents: ['defaults'],
+        examples: ['env-var'],
         type: 'map'
     },
     error_file: {
-        parents: ['api', 'endpoint', 'error_handler', 'files', 'server']
+        parents: ['api', 'endpoint', 'error_handler', 'files', 'server'],
+        examples: ['simple-fileserving']
     },
     expected_status: {
         parents: ['proxy', 'request'],
         type: 'array',
+        examples: ['sequences'],
         arrayType: 'number'
     },
     file: {
@@ -298,10 +324,12 @@ const attributes = {
         options: ['authorization_code', 'client_credentials']
     },
     header: {
-        parents: ['jwt']
+        parents: ['jwt'],
+        examples: ['jwt-access-control']
     },
     headers: {
         parents: ['jwt_signing_profile', 'request', 'response'],
+        examples: ['static-responses'],
         type: 'map'
     },
     health_path: {
@@ -326,7 +354,8 @@ const attributes = {
         type: 'array'
     },
     idp_metadata_file: {
-        parents: ['saml']
+        parents: ['saml'],
+        examples: ['saml']
     },
     ignore_request_violations: {
         parents: ['openapi'],
@@ -338,19 +367,23 @@ const attributes = {
     },
     json_body: {
         parents: ['request', 'response'],
+        examples: ['static-responses'],
         type: 'map'
     },
     jwks_ttl: {
         parents: ['jwt']
     },
     jwks_url: {
-        parents: ['jwt']
+        parents: ['jwt'],
+        examples: ['jwt-access-control']
     },
     key: {
-        parents: ['jwt', 'jwt_signing_profile']
+        parents: ['jwt', 'jwt_signing_profile'],
+        examples: ['jwt-access-control']
     },
     key_file: {
-        parents: ['jwt', 'jwt_signing_profile']
+        parents: ['jwt', 'jwt_signing_profile'],
+        examples: ['jwt-access-control']
     },
     log_format: {
         parents: ['settings']
@@ -391,6 +424,7 @@ const attributes = {
     },
     paths: {
         parents: ['spa'],
+        examples: ['spa-serving'],
         type: 'array'
     },
     proxy: {
@@ -404,7 +438,8 @@ const attributes = {
         parents: ['basic_auth']
     },
     redirect_uri: {
-        parents: ['beta_oauth2', 'oidc']
+        parents: ['beta_oauth2', 'oidc'],
+        examples: ['oidc']
     },
     remove_form_params: {
         parents: ['backend', 'endpoint', 'error_handler', 'proxy'],
@@ -412,6 +447,7 @@ const attributes = {
     },
     remove_query_params: {
         parents: ['backend', 'endpoint', 'error_handler', 'proxy'],
+        examples: ['query'],
         type: 'array'
     },
     remove_request_headers: {
@@ -439,6 +475,7 @@ const attributes = {
     },
     required_claims: {
         parents: ['jwt'],
+        examples: ['jwt-access-control'],
         type: 'array'
     },
     retries: {
@@ -457,10 +494,12 @@ const attributes = {
     },
     set_query_params: {
         parents: ['backend', 'endpoint', 'error_handler', 'proxy'],
+        examples: ['query'],
         type: 'map'
     },
     set_request_headers: {
         parents: ['backend', 'endpoint', 'error_handler', 'proxy', 'websockets'],
+        examples: ['sending-jwt-upstream'],
         type: 'map'
     },
     set_response_headers: {
@@ -473,7 +512,8 @@ const attributes = {
     },
     signature_algorithm: {
         parents: ['jwt', 'jwt_signing_profile'],
-        options: ['ES256', 'ES384', 'ES512', 'HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'RS512']
+        options: ['ES256', 'ES384', 'ES512', 'HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'RS512'],
+        examples: ['jwt-access-control']
     },
     signing_key: {
         parents: ['jwt']
@@ -485,13 +525,16 @@ const attributes = {
         parents: ['jwt']
     },
     sp_acs_url: {
-        parents: ['saml']
+        parents: ['saml'],
+        examples: ['saml'],
     },
     sp_entity_id: {
-        parents: ['saml']
+        parents: ['saml'],
+        examples: ['saml']
     },
     status: {
         parents: ['response'],
+        examples: ['static-responses'],
         type: 'number'
     },
     timeout: {
@@ -506,6 +549,7 @@ const attributes = {
     },
     token_value: {
         parents: ['jwt'],
+        examples: ['jwt-access-control'],
         type: 'any'
     },
     ttfb_timeout: {
@@ -544,16 +588,25 @@ const functions = {
     default: { description: 'Returns the first of the given arguments that is not null.' },
 	join: { description: 'Concatenates together the string elements of one or more lists with a given separator.' },
     json_decode: { description: 'Parses the given JSON string and, if it is valid, returns the value it represents.' },
-    json_encode: { description: 'Returns a JSON serialization of the given value.' },
+    json_encode: {
+        description: 'Returns a JSON serialization of the given value.',
+        examples: ['sending-jwt-upstream'],
+    },
     jwt_sign: { description: 'jwt_sign creates and signs a JSON Web Token (JWT) from information from a referenced jwt_signing_profile block and additional claims provided as a function parameter.' },
 	keys: { description: 'Takes a map and returns a sorted list of the map keys.' },
 	length: { description: 'Returns the number of elements in the given collection.' },
 	lookup: { description: 'Performs a dynamic lookup into a map.' },
     merge: { description: 'Deep-merges two or more of either objects or tuples. `null` arguments are ignored. A `null` attribute value in an object removes the previous attribute value. An attribute value with a different type than the current value is set as the new value. `merge()` with no parameters returns `null`.' },
-    oauth2_authorization_url: { description: 'Creates an OAuth2 authorization URL from a referenced OAuth2 AC Block or OIDC Block.' },
+    oauth2_authorization_url: {
+        description: 'Creates an OAuth2 authorization URL from a referenced OAuth2 AC Block or OIDC Block.',
+        examples: ['oidc']
+    },
     oauth2_verifier: { description: 'Creates a cryptographically random key as specified in RFC 7636, applicable for all verifier methods; e.g. to be set as a cookie and read into verifier_value. Multiple calls of this function in the same client request context return the same value.' },
     relative_url: { description: 'Returns a relative URL by retaining path, query and fragment components. The input URL must begin with `/<path>`, `//<authority>`, `http://` or `https://`, otherwise an error is thrown.' },
-    saml_sso_url: { description: 'Creates a SAML SingleSignOn URL (including the `SAMLRequest` parameter) from a referenced `saml` block.' },
+    saml_sso_url: {
+        description: 'Creates a SAML SingleSignOn URL (including the `SAMLRequest` parameter) from a referenced `saml` block.',
+        examples: ['saml']
+    },
 	set_intersection: { description: 'Returns a new set containing the elements that exist in all of the given sets.' },
     split: { description: 'Divides a given string by a given separator.' },
     substr: { description: 'Extracts a sequence of characters from another string.' },
@@ -616,6 +669,7 @@ const variables = {
     },
     env: {
         description: "The value of an environment variable.",
+        examples: ['env-var', 'docker-compose#environment-variables'],
         values: []
     },
     request: {
