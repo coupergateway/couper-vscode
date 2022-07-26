e0a85a99c2a0f4ef9723344f836abb9be155b56b
Joe Afflerbach <joe.afflerbach@avenga.com>
2022-07-26 13:55:15 +0200

    Upgrade ESLint to support the latest language features.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaVersion": 2022
     },
     "rules": {
       "no-unused-vars": "off"
