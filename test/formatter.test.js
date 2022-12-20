8118244aec4d8dd5d0930131b7533b577865e3f2
Johannes Koch <johannes.koch@avenga.com>
2022-12-20 12:54:42 +0100

    count occurences of opening and closing braces/parentheses/brackets and in-/decrease indentation level accordingly

--- test/formatter.test.js
+++ test/formatter.test.js
@@ -7,7 +7,12 @@ describe('Formatter', () => {
 	let testcases = [
 		['server{\n  }', ['server{', '}']],
 		['  server {\nendpoint "/" {\n}\n}', ['server {', '  endpoint "/" {', '  }', '}']],
-		['func({\nfoo = 1\n   })', ['func({', '  foo = 1', '})']]
+		['func({\nfoo = 1\n   })', ['func({', '    foo = 1', '})']],
+		['{\n  func("lbl", {\nfoo = 1\n   })\n}', ['{', '  func("lbl", {', '      foo = 1', '  })', '}']],
+		['{\n  func({\nfoo = 1\n   }\n)\n}', ['{', '  func({', '      foo = 1', '    }', '  )', '}']],
+		['{\nfunc(\n"lbl",\n{\nfoo = 1\n}\n)\n}', ['{', '  func(','    "lbl",', '    {', '      foo = 1', '    }', '  )', '}']],
+		['func({ foo = 1 },\n  "bar"\n)', ['func({ foo = 1 },', '  "bar"', ')']],
+		['jwt_sign(\n  "lbl",\n  {}\n  )', ['jwt_sign(', '  "lbl",', '  {}', ')']],
 	]
 
 	test.each(testcases)('%o', (text, expectedEdits) => {
